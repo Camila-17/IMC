0779f9f330513b3d455f56e849f21a7a6451bb13
Camila Duran <camilarxn023@gmail.com>
2022-03-29 14:31:00 -0500

    Actualizacion y detalles IMC

--- index.js
+++ index.js
@@ -8,23 +8,33 @@ Imc.addEventListener('click', function(){
     const resultado = document.querySelector('#resultadoImc');
 
     if(totalImc<18.5){
-        resultado.textContent='Usted esta bajo de peso'+totalImc.toFixed ;
-        imagenPeso.src='img/bajoPeso.jpg ', 'u-full-width';
+        resultado.textContent='Usted esta bajo de peso: '+totalImc.toFixed(1) ;
+        imagenPeso.src = 'img/bajoPeso.jpg ';
     }else{
         if(totalImc>= 18.5 && totalImc<=24.9){
-            
-            resultado.textContent='Usted esta saludable';
+            resultado.textContent='Usted esta saludable: '+totalImc.toFixed(1);
+            imagenPeso.src='img/pesoNormal.jpg';
         }else{
             if(totalImc>=25 && totalImc<=29.9){
-                resultado.textContent='Usted esta en sobrepeso';
+                resultado.textContent='Usted esta en sobrepeso: '+totalImc.toFixed(1);
+                imagenPeso.src='img/sobrePeso.jpg';
             }else{
                 if(totalImc>=30 && totalImc<=34.9){
-                    resultado.textContent='Usted esta obeso en nivel 1';
+                    resultado.textContent='Usted esta obeso en nivel uno: '+totalImc.toFixed(1);
+                    imagenPeso.src='img/obecidadaUno.jpg';
                 }else{
                     if(totalImc>=35 && totalImc<=39.9){
-                        resultado.textConten='Usted esta obeso en nivel 2';
+                        resultado.textConten='Usted esta obeso en nivel dos: '+totalImc.toFixed(1);
+                        imagenPeso.src='img/obecidadDos.jpg';
                     }else{
-                        resultado.textContent='Usted esta obeso en nivel 4';
+                        if(totalImc>=40.0){
+                            resultado.textContent = 'Usted esta obeso en nivel tres: ' +totalImc.toFixed(1);
+                            imagenPeso.src = 'img/obecidadTres.jpg';
+                        }else{
+                            if(isNaN('peso')||isNaN('altura')){
+                                resultado.textContent='No ingresó valores';
+                            }
+                        }
                     }
                 }
             }
